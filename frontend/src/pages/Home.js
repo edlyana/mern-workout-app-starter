7647c63b81bfbdc399ef335850bff123f27379be
NurEdlyana <23016282@myrp.edu.sg>
2025-01-20 15:01:04 +0800

    update

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import {useWorkoutsContext} from '../hooks/useWorkoutsContext'
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+
+const Home = () => {
+  const {workouts, dispatch} = useWorkoutsContext()
+
+  // Search Function
+  const [searchName, setSearchName] = useState('')
+  const filteredName = workouts ? workouts.filter((workout) => workout.title.toLowerCase().includes(searchName.toLowerCase())) : [];
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      // const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts`);
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <label style={{fontSize:"20px",color:"#127475"}}>Search Workouts:</label>
+            <input
+                type="text"
+                placeholder="Enter a workout"
+                value={searchName}
+                onChange = {(e) => setSearchName(e.target.value)}
+                style={{
+                    padding: "10px",
+                    fontSize: "16px",
+                    width: "80%",
+                    borderRadius: "5px",
+                    border: "1px solid #127475",
+                }}
+                className="searchBox"
+            />
+      <div className="workouts">
+        {filteredName.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
+}
+
+export default Home
