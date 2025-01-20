6c3c83ad11ea9b59d636ab109fe885597bc47778
NurEdlyana <23016282@myrp.edu.sg>
2025-01-20 16:25:28 +0800

    updated for delete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -8,6 +8,18 @@ const WorkoutDetails = ({ workout }) => {
 
   const handleClick = async () => {
     const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+  const handleChange = async () => {
+    const response = await fetch(
       `${process.env.REACT_APP_API_URL}/api/workouts/${workout.id}`, {
       method: 'DELETE'
     })
@@ -17,13 +29,14 @@ const WorkoutDetails = ({ workout }) => {
       dispatch({type: 'DELETE_WORKOUT', payload: json})
     }
   }
-  
+
     return (
       <div className="workout-details">
         <h4>{workout.title}</h4>
         <p><strong>Load (kg): </strong>{workout.load}</p>
         <p><strong>Number of reps: </strong>{workout.reps}</p>
         <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+        <span onClick={handleChange}>edit</span>
         <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
       </div>
     )
