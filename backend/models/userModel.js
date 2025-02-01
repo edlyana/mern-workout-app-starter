f153920e62a1a613aac1172fa5421085ad8df818
NurEdlyana <23016282@myrp.edu.sg>
2025-02-01 13:20:47 +0800

    updated error msgs

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,7 @@
 const bcrypt = require('bcrypt')
 
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -21,6 +22,18 @@ const userSchema = new Schema({
 
 //static signup method - userController.js signupUser method
 userSchema.statics.signup = async function(email, password) {
+
+    //validation
+    if (!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)){
+        throw Eroor('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)){
+        throw Error('Password is not strong enough')
+    }
+
     const exists = await this.findOne({ email })
 
     if (exists) {
