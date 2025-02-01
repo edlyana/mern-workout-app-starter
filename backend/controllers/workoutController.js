ae7cbf24a8e78b590f9b2cb97fb2bd340bf86917
NurEdlyana <23016282@myrp.edu.sg>
2025-02-01 20:53:14 +0800

    set specific workouts to display for diff users

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,20 +3,27 @@ const mongoose = require("mongoose");
 
 // Get all workouts
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 }); // descending order
+  const user_id = req.user._id
+
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 }); // descending order
+
   res.status(200).json(workouts);
 };
 
 // Get a single workout
 const getWorkout = async (req, res) => {
+
   const { id } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "Invalid ID" });
   }
+
   const workout = await Workout.findById(id);
   if (!workout) {
     return res.status(404).json({ error: "Workout not found" });
   }
+
   res.status(200).json(workout);
 };
 
@@ -43,41 +50,54 @@ const createWorkout = async (req, res) => {
   }
 
   try {
-    const workout = await Workout.create({ title, reps, load });
+    const user_id = req.user._id
+
+    const workout = await Workout.create({ title, reps, load, user_id });
     res.status(200).json(workout);
-  } catch (error) {
+  } 
+  catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
 // Delete a workout
 const deleteWorkout = async (req, res) => {
+
   const { id } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "Invalid ID" });
   }
+
   const workout = await Workout.findOneAndDelete({ _id: id });
+  
   if (!workout) {
     return res.status(400).json({ error: "Workout not found" });
   }
+
   res.status(200).json(workout);
 };
 
 // Update a workout
 const updateWorkout = async (req, res) => {
+
   const { id } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "Invalid ID" });
   }
+
   const workout = await Workout.findOneAndUpdate(
     { _id: id },
     {
       ...req.body,
     }
   );
+
   if (!workout) {
     return res.status(400).json({ error: "Workout not found" });
   }
+
   res.status(200).json(workout);
 };
 
